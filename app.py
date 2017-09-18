233a43d5e13175538b5122e1ed3540860f725320
Romack Natividad <romacknatividad@gmail.com>
2017-09-19 07:11:09 +0800

    Login using DB (dataset)

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-import os
+import os, dataset, MySQLdb
 from flask import Flask, request, session, redirect, url_for, escape
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
+db = dataset.connect('mysql://root:secret123@localhost/test')
+users = db['users']
+
 @app.route('/')
 def index():
-    if 'username' in session:
+    if 'user_id' in session:
         return 'Logged in as %s. <a href="/logout">Logout</a>' % escape(session['username'])
     return 'You are not logged in. <a href="/login">Login</a>'
 
@@ -14,17 +17,29 @@ def index():
 def login():
     if request.method == 'POST':
         username = request.form['username']
-        session['username'] = username
-        return redirect(url_for('index'))
+        password = request.form['password']
+        if user_exists(username, password):
+            return redirect(url_for('index'))
     return '''
     <form action="/login" method="POST">
         <p>Username <input type="text" name="username" /></p>
+        <p>Password <input type="password" name="password" /></p>
         <p><input type="submit" value="Login" /></p>
     </form>
     '''
 
+def user_exists(username, password):
+    row = users.find_one(username=username, pass_word=password)
+    session['user_id'] = row['id']
+    session['username'] = row['username']
+    if row == None:
+        return False
+    return True
+
+
 @app.route('/logout')
 def logout():
+    session.pop('user_id')
     session.pop('username')
     return redirect(url_for('index'))
 
